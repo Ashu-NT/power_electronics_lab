b5bba26435eb1fc48d4aaed01c6b91093a44f9ae
Ashu-NT <ashufrancis673@gmail.com>
2025-06-11 20:12:18 +0200

    "modi graph"

--- src/components/data_visualizing.py
+++ src/components/data_visualizing.py
@@ -146,6 +146,61 @@ def plot_multiple_sim_data(sim_data=None,  x_label='',title='',
     plt.tight_layout()
     return fig
 
+def plot_multiple_sim_data_2(sim_data=None,  x_label='',title='',
+                            y1_label='', y2_label='', y3_label='',
+                           label1 ="1st data",label2 ="2nd data",label3 ="3rd data"):
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax2 = ax1.twinx()
+    ax3 = ax1.twinx()
+    
+ 
+    try:
+        if sim_data is not None:
+            # Time and signal columns
+            t_sim = sim_data.iloc[1:, 0] * 1  # µs
+            x_sim = sim_data.iloc[1:, 1]       # V
+            y_sim = sim_data.iloc[1:, 2] * 1  # mA
+            z_sim = sim_data.iloc[1:, 3] * 1  # mW
+
+            # First Y-axis: 
+            ax1.plot(t_sim, x_sim, color='#aabb05', linestyle="-.", label=label1)
+            ax1.set_ylabel(y1_label, color='#aabb05')
+            ax1.tick_params(axis='y', labelcolor='#aabb05')
+            #ax1.set_yticks(np.arange(0,3.9,0.3))
+
+            # Second Y-axis: 
+          
+            ax2.plot(t_sim, y_sim, color='#aa0bcc', linestyle="-", label=label2)
+            ax2.set_ylabel(y2_label, color='#aa0bcc')
+            
+            ax2.tick_params(axis='y', labelcolor='#aa0bcc')
+
+            # Third Y-axis: 
+            
+            ax3.spines["right"].set_position(("axes", 1.15))  # shift third y-axis to the right
+            ax3.plot(t_sim, z_sim, color='#FF5733', linestyle="--", label=label3)
+            ax3.set_ylabel(y3_label, color='#FF5733')
+            #ax3.set_yticks(np.arange(0,150,15))
+            ax3.tick_params(axis='y', labelcolor='#FF5733')
+
+            # Add all legends to one box
+            lines = ax1.get_lines() + ax2.get_lines() + ax3.get_lines()
+            labels = [line.get_label() for line in lines]
+            ax1.legend(lines, labels, loc='center right',bbox_to_anchor=(1, 0.5),borderaxespad=0.)
+
+    except Exception as e:
+            logging.info(f"Error Plotting: {e}")
+
+    ax1.set_xlabel(x_label)
+    ax1.set_title(title)
+    ax1.grid(False)
+    ax2.grid(False)
+    ax3.grid(False)
+
+    plt.tight_layout()
+    return fig
+
+
 def plot_single_oscillo_data(ch1_data=None,ch2_data=None,  x_label='', y_label='', title='', label ="LTSpice Simulation"):
     fig, ax = plt.subplots()
     try:
